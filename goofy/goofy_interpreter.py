1f508ea99969498b7d909dd6f51ac8365c8c5220
Valla <hyperconix@pm.me>
2025-02-05 11:14:38 +0000

    feat(interpreter): add starting goofy interpreter code
    Goofy interpreter code which currently supports only the shove operation. Interpreting will return whether is is successful or not.

---
+++ goofy/goofy_interpreter.py
@@ -0,0 +1,82 @@
+from enum import Enum
+from goofy.goofy_tokenizer import GoofyTokenizer, TokenType
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+class SupportedOpcodes(Enum):
+    """ Represents the opcodes which
+    are supported in goofy.
+    """
+    SHOVE = "SHOVE"
+
+class GoofyInterpreter:
+    """ Responsible for doing the interpretation
+    of a goofy lang file, determining which operations
+    to perform.
+    """
+    def __init__(self, file_lines: list[str]):
+        self._stack: list[int] = []
+        
+        self.tokenizer = GoofyTokenizer(file_lines)
+        
+    def interpret(self) -> bool:
+        """ Responsible for interpreting the goofy file
+        lang that was provided when the interpreter object
+        was created. 
+
+        Returns:
+            bool: whether the interpreter was successful at parsing the tokens or not
+        """
+        tokens = self.tokenizer.tokenize()
+        
+        interpreting_succeded = True
+        
+        # We should never hit this case since this is handled when the file is read, but just in case something weird happens I've left it here.
+        if len(tokens) == 0: 
+            LOGGER.error("The token count is 0, which means the file provided was empty. Try another file")
+            
+            return not interpreting_succeded
+        
+        for i, token in enumerate(tokens):
+           if token.type == TokenType.OPCODE:
+               if not SupportedOpcodes.__contains__(token.value):
+                   LOGGER.error("Token %d:%s contains an unsupported opcode", i, token.value)
+                   
+                   return not interpreting_succeded
+               
+               # Where actual interpretation work begins: 
+               match token.value:
+                   case SupportedOpcodes.SHOVE.value:
+                        if len(tokens) > i + 1:
+                         next_token = tokens[i + 1]
+                        
+                         if not next_token.type == TokenType.INT_LITERAL:
+                            LOGGER.error("Token %d:%s is not an integer. SHOVE must be supplied an integer. Ex: SHOVE 3", i, token.value)
+                            
+                            return not interpreting_succeded
+                        
+                         self.stack.append(int(next_token.value))  
+                          
+                        else:
+                            LOGGER.error("There was no token to the right of SHOVE. Please make sure you remember to add a number")
+                            
+                            return not interpreting_succeded
+                             
+           elif token.type == TokenType.UNKNOWN:
+               LOGGER.error("Token %d:%s is an unknown token type. Please review the provided source", i, token.value)
+               
+               return not interpreting_succeded
+               
+        return interpreting_succeded    
+                                     
+    @property
+    def stack(self) -> list[int]:
+        """ Returns the current stack which
+        contains the values added during interpretation.
+
+        Returns:
+            list[int]: the current stack which contains 
+            the values added during interpretation.
+        """
+        return self._stack     
