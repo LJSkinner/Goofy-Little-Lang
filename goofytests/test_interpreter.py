dc450f7ebc3b1e15ddb01f89e9b21e841de41d2b
Valla <hyperconix@pm.me>
2025-02-05 11:47:58 +0000

    feat(tests): add tests for YEET opcode

--- goofytests/test_interpreter.py
+++ goofytests/test_interpreter.py
@@ -25,8 +25,11 @@ def test_interpreter_stack_contains_correct_values():
     assert len(stack) == 4
     
     assert stack[0] == 3
+    
     assert stack[1] == 4
+    
     assert stack[2] == 5
+    
     assert stack[3] == 6
     
     assert success
@@ -126,4 +129,91 @@ def test_interpreter_stack_operation_shove_unknown_token():
     assert len(stack) == 0
     
     assert not success
+
+def test_interpreter_stack_operation_yeet_stack_empty():
+    test_lines = ["YEET"]
     
+    interpreter = GoofyInterpreter(test_lines)
+    
+    success = interpreter.interpret()
+    
+    stack = interpreter.stack
+    
+    assert len(stack) == 0
+    
+    assert not success
+
+def test_interpreter_stack_operation_yeet_stack_popped():
+    test_lines = ["SHOVE 3 SHOVE 4 YEET"]
+    
+    interpreter = GoofyInterpreter(test_lines)
+    
+    success = interpreter.interpret()
+    
+    stack = interpreter.stack
+    
+    assert len(stack) == 1
+    
+    assert success
+    
+def test_interpreter_stack_operation_yeet_is_correct_1():
+    test_lines = ["SHOVE 3 SHOVE 4 YEET SHOVE 3 SHOVE 6 YEET"]
+    
+    interpreter = GoofyInterpreter(test_lines)
+    
+    success = interpreter.interpret()
+    
+    stack = interpreter.stack
+    
+    assert len(stack) == 2
+    
+    assert stack[0] == 1
+    
+    assert stack[1] == 3
+    
+    assert success
+
+def test_interpreter_stack_operation_yeet_is_correct_2():
+    test_lines = ["SHOVE 4 SHOVE 2 YEET"]
+    
+    interpreter = GoofyInterpreter(test_lines)
+    
+    success = interpreter.interpret()
+    
+    stack = interpreter.stack
+    
+    assert len(stack) == 1
+    
+    assert stack[0] == -2
+    
+    assert success
+    
+def test_interpreter_stack_operation_yeet_is_correct_3():
+    test_lines = ["SHOVE 40 SHOVE 50 YEET"]
+    
+    interpreter = GoofyInterpreter(test_lines)
+    
+    success = interpreter.interpret()
+    
+    stack = interpreter.stack
+    
+    assert len(stack) == 1
+    
+    assert stack[0] == 10
+    
+    assert success
+
+def test_interpreter_stack_operation_yeet_is_correct_4():
+    test_lines = ["SHOVE 3 SHOVE 4 YEET SHOVE 3 SHOVE 6 YEET YEET"]
+    
+    interpreter = GoofyInterpreter(test_lines)
+    
+    success = interpreter.interpret()
+    
+    stack = interpreter.stack
+    
+    assert len(stack) == 1
+    
+    assert stack[0] == 2
+
+    assert success
